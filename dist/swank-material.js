a1bded50751892e0da9a3ae03cb757f3756d8adf
Mark Litchfield <mlitchfield@timerazor.com>
2016-12-08 17:33:49 -0500

    Many template changes. Some logic updates and rendering updates.

--- dist/swank-material.js
+++ dist/swank-material.js
@@ -20,16 +20,22 @@
    */
   function SwankParseMDFilter($log) {
     return function(content) {
-      try {
-        if (!_.isObject(showdown)) {
-          throw new Error('Showdown library not found.');
+      if (typeof content === 'string') {
+        try {
+          if (!_.isObject(showdown)) {
+            throw new Error('Showdown library not found.');
+          }
+          var converter = new showdown.Converter({
+            ghCodeBlocks: true,
+            tasklists: true,
+            tables: true,
+            omitExtraWLInCodeBlocks: true
+          });
+          return converter.makeHtml(content);
+        } catch (err) {
+          $log.error(err);
+          return;
         }
-        showdown.setOption('tables', true);
-        var converter = new showdown.Converter();
-        return converter.makeHtml(content);
-      } catch (err) {
-        $log.error(err);
-        return;
       }
     };
   }
@@ -42,73 +48,101 @@
    * @param {Object} $rootScope Angular's $rootScope object
    */
   function SwankMaterialController(Swank, $scope, $rootScope) {
-    var _self = angular.extend(this, {
-      api: new Swank(this.api),
-      swankListener: $rootScope.$on('swankloaded', function() {
-        $scope.$broadcast('swankready');
-        _self.api.groupPaths(_self.groupBy);
-      })
+    var _self = angular.extend(this, { api: null, loading: true });
+    $rootScope.$on('swankloading', function() {
+      _self.loading = true;
     });
+    $rootScope.$on('swankloaded', function() {
+      $scope.$broadcast('swankready');
+      _self.loading = false;
+    });
+    _self.$onChanges = function(changed) {
+      if (changed.api.currentValue !== changed.api.previousValue) {
+        _self.api = new Swank(changed.api.currentValue);
+      }
+    };
   }
   SwankMaterialController.$inject = ['Swank', '$scope', '$rootScope'];
   var SwankMaterialComponent = {
     bindings: {api: '<', groupBy:'@?'},
     controller: SwankMaterialController,
     template: [
-      '<swank-material-info></swank-material-info>',
-        /*'<md-card-header>',
-          '<md-card-header-text layout="row">',
-            '<span class="md-headline">Swank - Material Design</span>',
-          '</md-card-header-text>',
-        '</md-card-header>',
-        '<md-card-content>',
-          '<pre>{{$ctrl.api | json}}</pre>',
-        '</md-card-content>',
-      '</md-card>'*/
+      '<md-content ng-if="$ctrl.loading" layout="row" layout-sm="column" layout-align="space-around">',
+        '<md-progress-circular md-mode="indeterminate"></md-progress-circular>',
+      '</md-content>',
+      '<md-content layout="row" layout-sm="column" ng-if="!$ctrl.loading">',
+        '<swank-material-info info="$ctrl.api.doc.info"></swank-material-info>',
+        '<swank-material-path ng-repeat="(route, path) in $ctrl.api.objects.paths" path="path" heading="route"></swank-material-path>',
+      '</md-content>'
     ].join('\n')
   };
 
-  function SwankMaterialInfoController() {}
   var SwankMaterialInfoComponent = {
-    bindings: {},
-    require: {
-      swank: '^swankMaterial'
-    },
+    bindings: {info:'<'},
+    require: { swank: '^swankMaterial' },
     template: [
       '<md-card>',
         '<md-card-header>',
           '<md-card-header-text layout="row">',
             '<span class="md-headline">',
-              '{{$ctrl.swank.api.doc.info.title}} ',
-              '<span class="md-caption">Version: {{$ctrl.swank.api.doc.info.version}}</span>',
+              '{{$ctrl.info.title}} ',
+              '<span class="md-caption" ng-if="$ctrl.info.version">Version: {{$ctrl.info.version}}</span>',
+            '</span>',
+            '<span flex></span>',
+            '<span ng-if="$ctrl.info.contact">',
+              '<span ng="$ctrl.info.contact.email">',
+                '<md-button type="button" class="md-icon-button" aria-label="Email" ng-href="mailto:{{$ctrl.info.contact.email}}">',
+                  '<md-tooltip>Email {{$ctrl.info.contact.name||\'\'}}</md-tooltip>',
+                  '<md-icon md-font-set="material-icons">mail</md-icon>',
+                '</md-button>',
+              '</span>',
+              '<span ng-if="$ctrl.info.contact.url">',
+                '<md-button type="button" class="md-icon-button" aria-label="Website" ng-href="{{$ctrl.info.contact.url}}" target="_blank">',
+                  '<md-tooltip>Visit {{$ctrl.info.contact.url}}</md-tooltip>',
+                  '<md-icon md-font-set="material-icons">open_in_browser</md-icon>',
+                '</md-button>',
+              '</span>',
             '</span>',
           '</md-card-header-text>',
         '</md-card-header>',
-        '<md-card-content>',
-          '<div ng-bind-html="$ctrl.swank.api.doc.info.description | parseMD"></div>',
-          /*'<pre>{{$ctrl.swank.api.doc.info | json}}</pre>',*/
+        '<md-card-content layout-padding ng-bind-html="$ctrl.info.description | parseMD"></md-card-content>',
+        '<md-card-actions layout="row" layout-align="end center">',
+          '<md-button ng-if="$ctrl.info.license.url" type="button" class="md-raised md-primary" aria-label="Released under {{$ctrl.info.license.name}}" ng-href="{{$ctrl.info.license.url}}" target="_blank">',
+            'Released under {{$ctrl.info.license.name}}',
+          '</md-button>',
+          '<md-button ng-if="$ctrl.info.termsOfService" type="button" class="md-raised md-primary" aria-label="Terms of Service" ng-href="{{$ctrl.info.termsOfService}}" target="_blank">',
+            'Terms of Service',
+          '</md-button>',
+        '</md-card-actions>',
+      '</md-card>'
+    ].join('\n')
+  };
+
+  var SwankMaterialPathComponent = {
+    bindings: {path:'<', heading:'<'},
+    require: { swank: '^swankMaterial' },
+    template: [
+      '<md-card>',
+        '<md-card-header>',
+          '<md-card-header-text layout="row">',
+            '<span class="md-headline">{{$ctrl.heading}}</span>',
+          '</md-card-header-text>',
+        '</md-card-header>',
+        '<md-card-content layout-padding>',
+          '<md-tabs md-dynamic-height md-border-bottom>',
+            '<md-tab ng-repeat="(method, operation) in $ctrl.path" label="{{method}}">',
+              '<pre>{{operation | json}}</pre>',
+            '</md-tab>',
+          '</md-tabs>',
         '</md-card-content>',
       '</md-card>'
     ].join('\n')
   };
-    /*"info": {
-      "description": "This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key `special-key` to test the authorization filters.",
-      "version": "1.0.0",
-      "title": "Swagger Petstore",
-      "termsOfService": "http://swagger.io/terms/",
-      "contact": {
-        "email": "apiteam@swagger.io"
-      },
-      "license": {
-        "name": "Apache 2.0",
-        "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
-      }
-    },*/
-
 
   angular.module('swank.material', ['swank'])
     .factory('_', LoDashFactory)
     .filter('parseMD', SwankParseMDFilter)
     .component('swankMaterial', SwankMaterialComponent)
-    .component('swankMaterialInfo', SwankMaterialInfoComponent);
+    .component('swankMaterialInfo', SwankMaterialInfoComponent)
+    .component('swankMaterialPath', SwankMaterialPathComponent);
 })(angular);
--- src/swank-material.js
+++ src/swank-material.js
@@ -20,16 +20,22 @@
    */
   function SwankParseMDFilter($log) {
     return function(content) {
-      try {
-        if (!_.isObject(showdown)) {
-          throw new Error('Showdown library not found.');
+      if (typeof content === 'string') {
+        try {
+          if (!_.isObject(showdown)) {
+            throw new Error('Showdown library not found.');
+          }
+          var converter = new showdown.Converter({
+            ghCodeBlocks: true,
+            tasklists: true,
+            tables: true,
+            omitExtraWLInCodeBlocks: true
+          });
+          return converter.makeHtml(content);
+        } catch (err) {
+          $log.error(err);
+          return;
         }
-        showdown.setOption('tables', true);
-        var converter = new showdown.Converter();
-        return converter.makeHtml(String(content).replace(/\s{2}/g, '\n\n'));
-      } catch (err) {
-        $log.error(err);
-        return;
       }
     };
   }
@@ -42,69 +48,74 @@
    * @param {Object} $rootScope Angular's $rootScope object
    */
   function SwankMaterialController(Swank, $scope, $rootScope) {
-    var _self = angular.extend(this, {
-      api: new Swank(this.api)
+    var _self = angular.extend(this, { api: null, loading: true });
+    $rootScope.$on('swankloading', function() {
+      _self.loading = true;
     });
     $rootScope.$on('swankloaded', function() {
       $scope.$broadcast('swankready');
-      _self.api.groupPaths(_self.groupBy);
+      _self.loading = false;
     });
+    _self.$onChanges = function(changed) {
+      if (changed.api.currentValue !== changed.api.previousValue) {
+        _self.api = new Swank(changed.api.currentValue);
+      }
+    };
   }
   SwankMaterialController.$inject = ['Swank', '$scope', '$rootScope'];
   var SwankMaterialComponent = {
     bindings: {api: '<', groupBy:'@?'},
     controller: SwankMaterialController,
     template: [
-      '<swank-material-info></swank-material-info>',
-      '<swank-material-path ng-repeat="(route, path) in $ctrl.api.objects.paths" path="path" heading="route"></swank-material-path>',
+      '<md-content ng-if="$ctrl.loading" layout="row" layout-sm="column" layout-align="space-around">',
+        '<md-progress-circular md-mode="indeterminate"></md-progress-circular>',
+      '</md-content>',
+      '<md-content layout="column" ng-if="!$ctrl.loading">',
+        '<swank-material-info info="$ctrl.api.doc.info"></swank-material-info>',
+        '<md-content layout="row">',
+          '<swank-material-listing list="$ctrl.api.doc.paths" flex="25"></swank-material-listing>',
+          '<md-content layout="column" flex="75">',
+            '<swank-material-path ng-repeat="(route, path) in $ctrl.api.doc.paths" path="path" heading="route"></swank-material-path>',
+          '</md-content>',
+        '</md-content>',
+      '</md-content>'
     ].join('\n')
   };
 
-  function SwankMaterialInfoController($scope) {
-    var _self = this;
-    $scope.$on('swankready', function() {
-      _self.infoBlock = _self.swank.api.doc.info;
-      console.log(_self.infoBlock.description);
-    });
-  }
-  SwankMaterialInfoController.$inject = ['$scope'];
   var SwankMaterialInfoComponent = {
-    bindings: {},
-    controller: SwankMaterialInfoController,
-    require: {
-      swank: '^swankMaterial'
-    },
+    bindings: {info:'<'},
+    require: { swank: '^swankMaterial' },
     template: [
       '<md-card>',
         '<md-card-header>',
           '<md-card-header-text layout="row">',
             '<span class="md-headline">',
-              '{{$ctrl.infoBlock.title}} ',
-              '<span class="md-caption">Version: {{$ctrl.infoBlock.version}}</span>',
+              '{{$ctrl.info.title}} ',
+              '<span class="md-caption" ng-if="$ctrl.info.version">Version: {{$ctrl.info.version}}</span>',
             '</span>',
             '<span flex></span>',
-            '<span ng-if="$ctrl.infoBlock.contact">',
-              '<span ng="$ctrl.infoBlock.contact.email">',
-                '<md-button type="button" class="md-icon-button" aria-label="Email" ng-href="mailto:{{$ctrl.infoBlock.contact.email}}">',
-                  '<md-tooltip>Email {{$ctrl.infoBlock.contact.name||\'\'}}</md-tooltip>',
+            '<span ng-if="$ctrl.info.contact">',
+              '<span ng="$ctrl.info.contact.email">',
+                '<md-button type="button" class="md-icon-button" aria-label="Email" ng-href="mailto:{{$ctrl.info.contact.email}}">',
+                  '<md-tooltip>Email {{$ctrl.info.contact.name||\'\'}}</md-tooltip>',
                   '<md-icon md-font-set="material-icons">mail</md-icon>',
                 '</md-button>',
               '</span>',
-              '<span ng-if="$ctrl.infoBlock.contact.url">',
-                '<md-button type="button" class="md-icon-button" aria-label="Website" ng-href="{{$ctrl.infoBlock.contact.url}}" target="_blank">',
-                  '<md-tooltip>Visit {{$ctrl.infoBlock.contact.url}}</md-tooltip>',
+              '<span ng-if="$ctrl.info.contact.url">',
+                '<md-button type="button" class="md-icon-button" aria-label="Website" ng-href="{{$ctrl.info.contact.url}}" target="_blank">',
+                  '<md-tooltip>Visit {{$ctrl.info.contact.url}}</md-tooltip>',
                   '<md-icon md-font-set="material-icons">open_in_browser</md-icon>',
                 '</md-button>',
               '</span>',
             '</span>',
           '</md-card-header-text>',
         '</md-card-header>',
-        '<md-card-content layout-padding ng-bind-html="$ctrl.infoBlock.description | parseMD"></md-card-content>',
+        '<md-card-content layout-padding ng-bind-html="$ctrl.info.description | parseMD"></md-card-content>',
         '<md-card-actions layout="row" layout-align="end center">',
-          '<md-button ng-if="$ctrl.infoBlock.license.url" type="button" class="md-raised md-primary" aria-label="Released under {{$ctrl.infoBlock.license.name}}" ng-href="{{$ctrl.infoBlock.license.url}}" target="_blank">',
-            'Released under {{$ctrl.infoBlock.license.name}}',
+          '<md-button ng-if="$ctrl.info.license.url" type="button" class="md-raised md-primary" aria-label="Released under {{$ctrl.info.license.name}}" ng-href="{{$ctrl.info.license.url}}" target="_blank">',
+            'Released under {{$ctrl.info.license.name}}',
           '</md-button>',
-          '<md-button ng-if="$ctrl.infoBlock.termsOfService" type="button" class="md-raised md-primary" aria-label="Terms of Service" ng-href="{{$ctrl.infoBlock.termsOfService}}" target="_blank">',
+          '<md-button ng-if="$ctrl.info.termsOfService" type="button" class="md-raised md-primary" aria-label="Terms of Service" ng-href="{{$ctrl.info.termsOfService}}" target="_blank">',
             'Terms of Service',
           '</md-button>',
         '</md-card-actions>',
@@ -112,28 +123,42 @@
     ].join('\n')
   };
 
+  var SwankMaterialListingComponent = {
+    bindings: { list:'=' },
+    require: { swank: '^swankMaterial' },
+    controller: function() {
+      this.slug = function(link) { return _.camelCase(link); };
+    },
+    template: [
+      '<md-card flex>',
+        '<md-list>',
+          '<md-list-item ng-repeat="(route, path) in $ctrl.list">',
+            '<div class="md-list-item-text">',
+              '<p><a href="#{{$ctrl.slug(route)}}">{{route}}</a></p>',
+            '</div>',
+          '</md-list-item>',
+        '</md-list>',
+      '</md-card>'
+    ].join('\n')
+  };
 
-  function SwankMaterialPathController($scope) {
-    var _self = this;
-  }
-  SwankMaterialPathController.$inject = ['$scope'];
   var SwankMaterialPathComponent = {
-    bindings: {path:'<', heading:'<'},
-    controller: SwankMaterialPathController,
-    require: {
-      swank: '^swankMaterial'
+    bindings: { path:'<', heading:'<' },
+    require: { swank: '^swankMaterial' },
+    controller: function() {
+      this.slug = function(link) { return _.camelCase(link); };
     },
     template: [
       '<md-card>',
         '<md-card-header>',
           '<md-card-header-text layout="row">',
-            '<span class="md-headline">{{$ctrl.heading}}</span>',
+            '<span class="md-headline"><a name="{{$ctrl.slug($ctrl.heading)}}">{{$ctrl.heading}}</a></span>',
           '</md-card-header-text>',
         '</md-card-header>',
         '<md-card-content layout-padding>',
           '<md-tabs md-dynamic-height md-border-bottom>',
             '<md-tab ng-repeat="(method, operation) in $ctrl.path" label="{{method}}">',
-              '<pre>{{operation | json}}</pre>',
+              '<swank-material-path-detail detail="operation"></swank-material-path-detail>',
             '</md-tab>',
           '</md-tabs>',
         '</md-card-content>',
@@ -141,23 +166,50 @@
     ].join('\n')
   };
 
-  /*function SwankMaterialOperationController($scope) {}
-  SwankMaterialOperationController.$inject = ['$scope'];
-  var SwankMaterialOperationComponent = {
-    bindings:{},
-    controller: SwankMaterialOperationController,
-    require:{
-      swank: '^swankMaterial'
-    },
+  var SwankMaterialPathDetailComponent = {
+    bindings: { detail: '<' },
+    require: { path: '^swankMaterialPath' },
     template: [
-
-    ].joi('\n')
-  };*/
+      '<div>',
+        '<p><strong>Description:</strong><br /><span ng-bind-html="$ctrl.detail.description | parseMD"></span></p>',
+        '<p ng-if="$ctrl.detail.tags"><strong>Tags: </strong>{{$ctrl.detail.tags.join(\',\')}}</p>',
+        '<p ng-if="$ctrl.detail.produces"><strong>Outputs: </strong>{{$ctrl.detail.produces.join(\',\')}}</p>',
+        '<md-list ng-if="$ctrl.detail.parameters && $ctrl.detail.parameters.length>0">',
+          '<md-subheader class="md-no-sticky">Parameters:</md-subheader>',
+          '<md-list-item class="md-3-line" ng-repeat="parameter in $ctrl.detail.parameters">',
+            '<div class="md-list-item-text">',
+              '<h3><small>[{{parameter.required?\'required\':\'optional\'}}]</small> <strong>{{parameter.name}}</strong></h3>',
+              '<h4>{{parameter.description}}</h4>',
+              '<p>Parameter placement: {{parameter.in}}, Type: {{parameter.type}}</p>',
+            '</div>',
+          '</md-list-item>',
+        '</md-list>',
+        '<md-list>',
+          '<md-subheader class="md-no-sticky">Responses:</md-subheader>',
+          '<md-list-item class="md-3-line" ng-repeat="(statuscode, response) in $ctrl.detail.responses">',
+            '<div class="md-list-item-text">',
+              '<h3><strong>{{statuscode}}</strong></h3>',
+              '<h4>{{response.description || \'No description available.\'}}</h4>',
+              '<span ng-if="response.examples">',
+                '<p ng-repeat="(mime, example) in response.examples">',
+                  '<strong>{{mime}}</strong><br />',
+                  '<code style="white-space:pre-wrap;">{{example}}</code>',
+                '</p>',
+              '</span>',
+            '</div>',
+          '</md-list-item>',
+        '</md-list>',
+        /*'<pre>{{$ctrl.detail | json}}</pre>',*/
+      '</div>'
+    ].join('\n')
+  };
 
   angular.module('swank.material', ['swank'])
     .factory('_', LoDashFactory)
     .filter('parseMD', SwankParseMDFilter)
     .component('swankMaterial', SwankMaterialComponent)
     .component('swankMaterialInfo', SwankMaterialInfoComponent)
-    .component('swankMaterialPath', SwankMaterialPathComponent);
+    .component('swankMaterialListing', SwankMaterialListingComponent)
+    .component('swankMaterialPath', SwankMaterialPathComponent)
+    .component('swankMaterialPathDetail', SwankMaterialPathDetailComponent);
 })(angular);
